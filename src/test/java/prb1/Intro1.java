fcc0b07fd2072c6feac3f4e00d73d0503494addd
shaikshakeerss123 <shaikshakeerss123@gmail.com>
2024-10-17 12:01:27 +0530

    commit-1

---
+++ src/test/java/prb1/Intro1.java
@@ -0,0 +1,10 @@
+package prb1;
+
+public class Intro1 {
+
+	public static void main(String[] args) {
+		System.out.println("Basic Checking for github pushing");
+
+	}
+
+}
---
+++ src/test/java/prb2/LoopIntro1.java
@@ -0,0 +1,13 @@
+package prb2;
+
+public class LoopIntro1 {
+
+	public static void main(String[] args) {
+		int arr []= {1,2,5,4,6};
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
